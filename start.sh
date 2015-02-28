ed45ae21947fad8350da419e925eb4cf8ade1670
cpdc <cccpdc@163.com>
2015-02-28 18:04:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # current data storage path should have write permission
-chmod o+w /influxdb_data/
+if [ ! -d "/influxdb_data"]; then 
+  mkdir /influxdb_data 
+fi
+
+chmod o+w /influxdb_data
 
 /etc/init.d/influxdb start
 /usr/sbin/nginx -c /etc/nginx/nginx.conf
