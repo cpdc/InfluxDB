f69e7d93f53e07eb4a689563f93eb66b1d11e1f8
cpdc <cccpdc@163.com>
2015-03-05 15:01:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,5 +10,8 @@ chmod o+w /opt/influxdb/shared/data/
 /etc/init.d/influxdb start
 /usr/sbin/nginx -c /etc/nginx/nginx.conf
 
+export LOCAL_IP=`ip addr | grep "eno1" | grep "inet" | cut -f 1 -d "/"|cut -f 6 -d " "`
+sed -i "s/localhost/$LOCAL_IP/g" grafana-1.9.1/config.js
+
 # both influxdb and nginx run in daemon, such that we have to block the current terminal to avoid exit
 sleep 100000d
